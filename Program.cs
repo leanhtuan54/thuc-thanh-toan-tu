1c46c90891dadff08051c4b7039de9db018ba0e4
leanhtuan54 <anhtuanle0054@gmail.com>
2024-08-05 21:19:47 +0700

    add bait thuc hanh

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           float width;
+           float height;
+
+           Console.Write("Nhap chieu cao: ");
+           width = float.Parse(Console.ReadLine());
+
+           Console.Write("Nhap chieu dai: ");
+           height = float.Parse(Console.ReadLine());
+
+           float area = width * height;
+           Console.WriteLine("Dien tich hinh chu nhat: " + area);
+        }
+    }
+}
